a1b35df3b1dd6b4eb026889675cd5ea45ae1d949
leti9302 <alexpskov60@gmail.com>
2020-12-04 03:26:15 +0300

    Almost

--- Fractal.cpp
+++ Fractal.cpp
@@ -1,26 +1,4 @@
-#include <sstream>
-#include <ctime>
-
-using namespace Magick;
-using namespace std;
-
-struct Coordinates
-{
-	int x, y;
-};
-
-struct AffineTransform
-{
-	Coordinates position;
-	int status;
-};
-
-struct DomainImageList
-{
-	Image domain;
-	Coordinates position;
-	DomainImageList* next;
-};
+#include "Header.h";
 
 void DILappend(DomainImageList** head, DomainImageList* newElem)
 {
@@ -29,13 +7,18 @@ void DILappend(DomainImageList** head, DomainImageList* newElem)
 	cur->next = newElem;
 }
 
-void bestStatus(Image range, DomainImageList** all_domain_blocks, AffineTransform* cur_affine_transform)
+void bestStatus(Image range, DomainImageList** all_domain_blocks, AffineTransform* best_affine_transform) //лучший доменный блок для посылаемого рангового (перебор всех доменных)
 {
 	double factorRMSEM, bestfactorRMSEM = 1;
-	DomainImageList* cur = *all_domain_blocks;
-	while (cur->next != nullptr)
+	BrightnessShiftRGB temp_brightness_shift;
+	DomainImageList* cur_domain_list = *all_domain_blocks;
+	Image domain;
+
+	while (cur_domain_list->next != nullptr)
 	{
-		Image domain = cur->domain;
+		domain = cur_domain_list->domain;
+		brightnessShift(range, domain, &temp_brightness_shift);
+		applyColorTransformation(&domain, temp_brightness_shift);
 		for (int i = 0; i < 8; i++)
 		{
 			if (i == 4) domain.flop();
@@ -43,14 +26,68 @@ void bestStatus(Image range, DomainImageList** all_domain_blocks, AffineTransfor
 			if (bestfactorRMSEM > factorRMSEM)
 			{
 				bestfactorRMSEM = factorRMSEM;
-				cur_affine_transform->position.x = cur->position.x;
-				cur_affine_transform->position.y = cur->position.y;
-				cur_affine_transform->status = i;
+				best_affine_transform->brightness_shift.red = temp_brightness_shift.red;
+				best_affine_transform->brightness_shift.green = temp_brightness_shift.green;
+				best_affine_transform->brightness_shift.blue = temp_brightness_shift.blue;
+				best_affine_transform->position.x = cur_domain_list->position.x;
+				best_affine_transform->position.y = cur_domain_list->position.y;
+				best_affine_transform->status = i;
 			}
 			domain.rotate(90);
 		}
-		cur = cur->next;
+		cur_domain_list = cur_domain_list->next;
 	}
+}
+
+void applyColorTransformation(Image* domain, BrightnessShiftRGB shift)
+{
+	double coef_compress = 0.75;
+	ColorRGB current;
+	double current_red, current_green, current_blue;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			current = domain->pixelColor(i, j);
+			current_red = current.red();
+			current_green = current.green();
+			current_blue = current.blue();
+			current_red = current_red * coef_compress + shift.red;
+			current_green = current_green * coef_compress + shift.green;
+			current_blue = current_blue * coef_compress + shift.blue;
+			domain->pixelColor(i, j, ColorRGB(current_red, current_green, current_blue));
+		}
+	}
+}
+
+void brightnessShift(Image range, Image domain, BrightnessShiftRGB* brightness_shift_RGB)
+{
+	double coef_compress = 0.75;
+	int i, j;
+	ColorRGB cursor_range, cursor_domain;
+
+	brightness_shift_RGB->red = 0, brightness_shift_RGB->green = 0, brightness_shift_RGB->blue = 0;
+
+
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			cursor_range = range.pixelColor(i, j);
+			cursor_domain = domain.pixelColor(i, j);
+			brightness_shift_RGB->red = brightness_shift_RGB->red + cursor_range.red() - coef_compress * cursor_domain.red();
+			brightness_shift_RGB->green = brightness_shift_RGB->green + cursor_range.green() - coef_compress * cursor_domain.green();
+			brightness_shift_RGB->blue = brightness_shift_RGB->blue + cursor_range.blue() - coef_compress * cursor_domain.blue();
+
+		}
+
+	}
+	brightness_shift_RGB->red /= 16;
+	brightness_shift_RGB->green /= 16;
+	brightness_shift_RGB->blue /= 16;
+
+
+
 }
 
 string forResize(int width, int height)
@@ -71,17 +108,36 @@ string forCrop(int width, int height, int k)
 	return str;
 }
 
+void file_result(ofstream& out, AffineTransform** table_best_match, int width, int height)
+{
+	int i, j;
+	out << width << ' ' << height << '\n';
+	for (j = 0; j < height / 4; j++)
+	{
+		for (i = 0; i < width / 4; i++)
+		{
+			out << table_best_match[j][i].position.x << ' ' << table_best_match[j][i].position.y << ' ';
+			out << table_best_match[j][i].brightness_shift.red << ' ' << table_best_match[j][i].brightness_shift.green << ' ' << table_best_match[j][i].brightness_shift.blue << ' ';
+			out << table_best_match[j][i].status << '\n';
+		}
+	}
+
+}
+
+
 int main(int argc, char** argv) {
 	try {
 		InitializeMagick(*argv);
+		ofstream compress_result;
+		compress_result.open("Compress Image.txt");
 		Image image("lena512.bmp");
-		image.resize("32x32!");
+		image.resize("16x16!");
 		Image domain_full, domain, range;
 		DomainImageList* all_domain_blocks = new DomainImageList;
 		all_domain_blocks = nullptr;
 		AffineTransform** table_best_match;
 		AffineTransform* cur_affine_transform = new AffineTransform;
-		int width, height, i_table, j_table, x, y;
+		int width, height, i_table, j_table, x, y, i, j;
 		width = image.columns();
 		height = image.rows();
 
@@ -115,8 +171,8 @@ int main(int argc, char** argv) {
 			}
 		}
 
-		int start_clock, end_clock;
-		start_clock = clock();
+		/*int start_clock, end_clock;
+		start_clock = clock();*/
 		for (j_table = 0; j_table < height / 4; j_table++) //j_table < height/4; j_table++)
 		{
 			for (i_table = 0; i_table < width / 4; i_table++) //width/4; i_table++) //проход по ранговым (ячейкам таблицы)
@@ -130,20 +186,23 @@ int main(int argc, char** argv) {
 
 				table_best_match[j_table][i_table] = *cur_affine_transform;
 
-				cout << "(" << table_best_match[j_table][i_table].position.x << ',' << table_best_match[j_table][i_table].position.y << ") ";
-				cout << table_best_match[j_table][i_table].status << ' ' << " (" << j_table * 4 << ',' << i_table * 4 << ")\n";
+				/*cout << "(" << table_best_match[j_table][i_table].position.x << ',' << table_best_match[j_table][i_table].position.y << ") ";
+				cout << table_best_match[j_table][i_table].status << ' ' << " (" << j_table * 4 << ',' << i_table * 4 << ")\n";*/
 			}
 		}
-		end_clock = clock();
-		cout << "_________________________________\n";
-		for (int i = 0; i < 4; i++)
-		{
-			cout << "(" << table_best_match[0][i].position.x << ',' << table_best_match[0][i].position.y << ") ";
-			cout << table_best_match[0][i].status << ' ';
-			cout << "(" << 0 << ',' << i * 4 << ")\n";
-		}
-		cout << double(end_clock - start_clock) / double(CLOCKS_PER_SEC) << endl;
-		system("pause");
+		//end_clock = clock();
+		//cout << "_________________________________\n";
+		//for (int i = 0; i < 4; i++)
+		//{
+		//	cout << "(" << table_best_match[0][i].position.x << ',' << table_best_match[0][i].position.y << ") ";
+		//	cout << table_best_match[0][i].status << ' ';
+		//	cout << "(" << 0 << ',' << i * 4 << ")\n";
+		//}
+		//cout << double(end_clock - start_clock) / double(CLOCKS_PER_SEC) << endl;
+		//system("pause");
+		file_result(compress_result, table_best_match, width, height);
+		compress_result.close();
+
 	}
 	catch (Magick::Exception& error) {
 		cerr << "Caught Magick++ exception: " << error.what() << endl;
---
+++ Header.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <Magick++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+
+using namespace Magick;
+using namespace std;
+
+struct Coordinates
+{
+	int x, y;
+};
+
+struct BrightnessShiftRGB
+{
+	double red;
+	double green;
+	double blue;
+};
+
+struct AffineTransform
+{
+	Coordinates position;
+	BrightnessShiftRGB brightness_shift;
+	short int status;
+};
+
+struct DomainImageList
+{
+	Image domain;
+	Coordinates position;
+	DomainImageList* next;
+};
+void applyColorTransformation(Image*, BrightnessShiftRGB);
+void DILappend(DomainImageList**, DomainImageList*);
+void bestStatus(Image, DomainImageList**, AffineTransform*);
+void brightnessShift(Image, Image, BrightnessShiftRGB*);
+string forResize(int, int);
+string forCrop(int, int, int);
+void file_result(ofstream&, AffineTransform**, int, int);
